adff2674ad9bc343eab5f4727b9bdb9acfb8afef
Jungir <googlman6300@gmail.com>
2019-09-26 09:19:33 +0600

    fetching data for pages

---
+++ pages/index.clean.url.js
@@ -0,0 +1,25 @@
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+//href att will look at the pages folder
+//as is an allias to show in the URL bar
+const PostLink = props => (
+  <li>
+    <Link href="/p/[id]" as={`/p/${props.id}`}>
+      <a>{props.id}</a>
+    </Link>
+  </li>
+);
+
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" />
+        <PostLink id="learn-nextjs" />
+        <PostLink id="deploy-nextjs" />
+      </ul>
+    </Layout>
+  );
+}
